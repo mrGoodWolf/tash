a87d518dd01ed46319fd9b11a82b9f1d5b1734a3
mrGoodWolf <138707027+mrGoodWolf@users.noreply.github.com>
2025-02-19 14:57:39 -0600

    Imported tash.c from local.

--- tash.c
+++ tash.c
@@ -1,1 +1,275 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+/*
+  Function Declarations for builtin shell commands:
+ */
+int cd(char **args);
+int help(char **args);
+int tash_exit(char **args);
+
+/*
+  List of builtin commands, followed by their corresponding functions.
+ */
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+  &cd,
+  &help,
+  &exit
+};
+
+int tash_num_builtins() {
+  return sizeof(builtin_str) / sizeof(char *);
+}
+
+/*
+  Builtin function implementations.
+*/
+
+/**
+   @brief Builtin command: change directory.
+   @param args List of args.  args[0] is "cd".  args[1] is the directory.
+   @return Always returns 1, to continue executing.
+ */
+int cd(char **args)
+{
+  if (args[1] == NULL) {
+    fprintf(stderr, "tash: expected argument to \"cd\"\n");
+  } else {
+    if (chdir(args[1]) != 0) {
+      perror("tash");
+    }
+  }
+  return 1;
+}
+
+/**
+   @brief Builtin command: print help.
+   @param args List of args.  Not examined.
+   @return Always returns 1, to continue executing.
+ */
+int help(char **args)
+{
+  int i;
+  printf("The Amazing SHell:TASH!\n");
+  printf("Type program names and arguments, and hit enter.\n");
+  printf("The following are built in:\n");
+
+  for (i = 0; i < tash_num_builtins(); i++) {
+    printf("  %s\n", builtin_str[i]);
+  }
+
+  printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+/**
+   @brief Builtin command: exit.
+   @param args List of args.  Not examined.
+   @return Always returns 0, to terminate execution.
+ */
+int tash_exit(char **args)
+{
+  return 0;
+}
+
+/**
+  @brief Launch a program and wait for it to terminate.
+  @param args Null terminated list of arguments (including program).
+  @return Always returns 1, to continue execution.
+ */
+int tash_launch(char **args)
+{
+  pid_t pid;
+  int status;
+
+  pid = fork();
+  if (pid == 0) {
+    // Child process
+    if (execvp(args[0], args) == -1) {
+      perror("tash");
+    }
+    tash_exit(EXIT_FAILURE);
+  } else if (pid < 0) {
+    // Error forking
+    perror("tash");
+  } else {
+    // Parent process
+    do {
+      waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+
+  return 1;
+}
+
+/**
+   @brief Execute shell built-in or launch program.
+   @param args Null terminated list of arguments.
+   @return 1 if the shell should continue running, 0 if it should terminate
+ */
+int tash_execute(char **args)
+{
+  printf("\033[0;33m");
+  int i;
+
+  if (args[0] == NULL) {
+    // An empty command was entered.
+    return 1;
+  }
+
+  for (i = 0; i < tash_num_builtins(); i++) {
+    if (strcmp(args[0], builtin_str[i]) == 0) {
+      return (*builtin_func[i])(args);
+    }
+  }
+
+  return tash_launch(args);
+}
+
+/**
+   @brief Read a line of input from stdin.
+   @return The line from stdin.
+ */
+char *tash_read_line(void)
+{
+#ifdef tash_USE_STD_GETLINE
+  char *line = NULL;
+  ssize_t bufsize = 0; // have getline allocate a buffer for us
+  if (getline(&line, &bufsize, stdin) == -1) {
+    if (feof(stdin)) {
+      tash_exit(EXIT_SUCCESS);  // We received an EOF
+    } else  {
+      perror("tash: getline\n");
+      tash_exit(EXIT_FAILURE);
+    }
+  }
+  return line;
+#else
+#define tash_RL_BUFSIZE 1024
+  int bufsize = tash_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * bufsize);
+  int c;
+
+  if (!buffer) {
+    fprintf(stderr, "tash: allocation error\n");
+    tash_exit(EXIT_FAILURE);
+  }
+
+  while (1) {
+    // Read a character
+    c = getchar();
+
+    if (c == EOF) {
+      tash_exit(EXIT_SUCCESS);
+    } else if (c == '\n') {
+      buffer[position] = '\0';
+      return buffer;
+    } else {
+      buffer[position] = c;
+    }
+    position++;
+
+    // If we have exceeded the buffer, reallocate.
+    if (position >= bufsize) {
+      bufsize += tash_RL_BUFSIZE;
+      buffer = realloc(buffer, bufsize);
+      if (!buffer) {
+        fprintf(stderr, "tash: allocation error\n");
+        tash_exit(EXIT_FAILURE);
+      }
+    }
+  }
+#endif
+}
+
+#define tash_TOK_BUFSIZE 64
+#define tash_TOK_DELIM " \t\r\n\a"
+/**
+   @brief Split a line into tokens (very naively).
+   @param line The line.
+   @return Null-terminated array of tokens.
+ */
+char **tash_split_line(char *line)
+{
+  int bufsize = tash_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char*));
+  char *token, **tokens_backup;
+
+  if (!tokens) {
+    fprintf(stderr, "tash: allocation error\n");
+    tash_exit(EXIT_FAILURE);
+  }
+
+  token = strtok(line, tash_TOK_DELIM);
+  while (token != NULL) {
+    tokens[position] = token;
+    position++;
+
+    if (position >= bufsize) {
+      bufsize += tash_TOK_BUFSIZE;
+      tokens_backup = tokens;
+      tokens = realloc(tokens, bufsize * sizeof(char*));
+      if (!tokens) {
+		free(tokens_backup);
+        fprintf(stderr, "tash: allocation error\n");
+        tash_exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, tash_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
+}
+
+/**
+   @brief Loop getting input and executing it.
+ */
+void tash_loop(void)
+{
+  char *line;
+  char **args;
+  int status;
+
+  do {
+    printf("\033[0;35m");
+    printf("tash");
+    printf("\033[0;31m");
+    printf("> ");
+    printf("\033[0;36m");
+    line = tash_read_line();
+    args = tash_split_line(line);
+    printf("\033[0;33m");
+    status = tash_execute(args);
+
+    free(line);
+    free(args);
+  } while (status);
+}
+
+/**
+   @brief Main entry point.
+   @param argc Argument count.
+   @param argv Argument vector.
+   @return status code
+ */
+int main(int argc, char **argv)
+{
+  // Load config files, if any.
+
+  // Run command loop.
+  tash_loop();
+
+  // Perform any shutdown/cleanup.
+
+  return EXIT_SUCCESS;
+}
